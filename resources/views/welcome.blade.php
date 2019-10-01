10b289687ab66b0c3ed3cf2384e7828865b64c65
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2019-10-01 16:27:14 -0500

    cambie el saludo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola
+                    HOLA DE NUEVO
                 </div>
 
                 <div class="links">
